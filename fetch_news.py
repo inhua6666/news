37b018006a863f0fdf877d6894be32ff1a450ee9
inhua6666 <criswu965@gmail.com>
2026-09-21 16:50:01 +0800

    Update RSS sources for news fetching

--- fetch_news.py
+++ fetch_news.py
@@ -10,13 +10,13 @@ import os
 # Server酱配置 - 从环境变量获取
 SEND_KEY = os.environ.get("SEND_KEY", "")
 
-# 世界热点 / 国际局势 RSS源
+# 世界# 世界热点 / 国际局势（中文源）
 RSS_SOURCES = [
     {"name": "BBC中文", "url": "https://www.bbc.com/zhongwen/simp/rss.xml"},
-    {"name": "新华社", "url": "http://www.xinhuanet.com/politics/news_politics.xml"},
-    {"name": "环球时报", "url": "https://www.huanqiu.com/rss"},
-    {"name": "Reuters世界", "url": "https://www.reutersagency.com/feed/?taxonomy=best-topics&post_type=best"},
-    {"name": "Al Jazeera", "url": "https://www.aljazeera.com/xml/rss/all.xml"},
+    {"name": "新华社国际", "url": "http://www.news.cn/world/news_world.xml"},
+    {"name": "环球网", "url": "https://www.huanqiu.com/rss"},
+    {"name": "观察者网", "url": "https://www.guancha.cn/rss"},
+    {"name": "参考消息", "url": "https://www.cankaoxiaoxi.com/rss"},
 ]
 def get_rss_news(source, hours=24):
     """抓取RSS源过去24小时的新闻"""
